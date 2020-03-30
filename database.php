d50a8ca2964aa6ebb60a25e3ee74c0ec5ae14e50
nandocg <50077180+nandocg@users.noreply.github.com>
2020-03-30 14:02:49 +0700

    Update database.php

--- database.php
+++ database.php
@@ -1,7 +1,7 @@
 <?php
 $host = 'localhost';
 $username = 'root';
-$password = "cloudweek6";
+$password = "ubayaweek6";
 $dbname = "mydb";
 $conn = new mysqli($host,$username,$password,$dbname);
 if($conn->connect_error){
