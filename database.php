afc6613785600bbd86d6c32ec6dc3a1d2d6419b0
nandocg <50077180+nandocg@users.noreply.github.com>
2020-03-30 14:34:12 +0700

    Update database.php

--- database.php
+++ database.php
@@ -1,7 +1,7 @@
 <?php
 $host = getenv("MYSQL_SERVICE_HOST");
 $username = 'root';
-$password = "ubaya";
+$password = "";
 $dbname = "mydb";
 $conn = new mysqli($host,$username,$password,$dbname);
 if($conn->connect_error){
