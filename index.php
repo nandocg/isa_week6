2bdcb3337a1beee2001fa236626ca77541df8246
nandocg <50077180+nandocg@users.noreply.github.com>
2020-03-31 21:38:44 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,24 +3,47 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Week 6 ISA</title>
+	<style>
+		table,td,th{
+			border: 1px solid black;
+			border-collapse: collapse;
+			text-align: center;
+		}
+		
+		table{
+			width: 80%;
+			margin: auto;
+		}
+		td{
+			padding: 10px;
+		}
+		tr:nth-child(even)
+		{
+			background: beige;
+		}
+
+	</style>
 </head>
 <body>
 	<?php
 	 require_once('database.php');
 	 ?>
 
-	 <h2>Taxi Online</h2>
+	 <h2 style="text-align: center;">Taxi Online</h2>
 	 <table>
 	 	<tr>
-	 		<tr>ID</tr>
-	 		<tr>Name</tr>
-	 		<tr>License</tr>
+	 		<th>ID</th>
+	 		<th>Name</th>
+	 		<th>License</th>
 	 	</tr>
 	 	<?php
 	 		$query = "SELECT * FROM driver";
 	 		$result = $conn->query($query);
-	 		$rows = $result->fetch_assoc();
-	 		var_dump($rows);
+
+	 		while($rows = $result->fetch_assoc())
+	 		{
+	 			echo "<tr><td>".$rows['id']."</td><td>".$rows['name']."</td><td>".$rows['license']."</td></tr>";
+	 		}
 	 	?>
 	 </table>
 </body>
