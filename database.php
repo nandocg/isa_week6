97d3b0b8e5cf626bb707901fc678b8d905d6dc80
nandocg <50077180+nandocg@users.noreply.github.com>
2020-03-31 22:07:53 +0700

    Update database.php

--- database.php
+++ database.php
@@ -2,7 +2,7 @@
 $host = getenv('MYSQL_SERVICE_HOST');
 $username = 'root';
 $password = 'ubaya';
-$dbname = 'sampledb';
+$dbname = 'databaseku';
 $conn = new mysqli($host,$username,$password,$dbname);
 if($conn->connect_error){
 	echo "Connection error";
